025ff7883c3e836906b4ed3f6705709e599334e4
Nguyen Quoc Do <nguyenquocdodary@gmail.com>
2025-05-30 19:53:55 +0700

    Initial commit for Product Details microservice

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = [
+        {"id": 1, "name": "Product A"},
+        {"id": 2, "name": "Product B"}
+    ]
+    return jsonify(products)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
